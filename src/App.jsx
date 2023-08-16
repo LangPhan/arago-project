b813be27b3c506c37e884949027904fa3c04ce73
LangPhan <langphankk@gmail.com>
2023-08-16 15:18:00 +0700

    Init Project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Text } from "@aws-amplify/ui-react";
+
+function App() {
+  return (
+    <>
+      <h2 className="text-blue-500">Hello World</h2>
+      <Text className="text-blue-600">Hello World</Text>
+    </>
+  );
+}
+
+export default App;
